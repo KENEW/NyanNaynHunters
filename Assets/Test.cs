5b3921400ba75fe787afb6ec1311ec6d9f75f2d4
GodHongIl <adunstudio@daum.net>
2020-09-08 21:59:50 +0900

    BG데이터베이스

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log(TBL_MOVE_CARD.CountEntities); // 3
+        int count = TBL_MOVE_CARD.CountEntities;
+
+        for (int i = 0; i < count; ++i)
+        {
+            TBL_MOVE_CARD data = TBL_MOVE_CARD.GetEntity(i);
+            Debug.Log(data.Name);
+            Debug.Log(data.Distance);
+            Debug.Log(data.Comment);
+        }
+        
+        TBL_MOVE_CARD.ForEachEntity((data) =>
+        {
+            Debug.Log(data.Name);
+            Debug.Log(data.Distance);
+            Debug.Log(data.Comment);
+        });
+
+
+        var data3st = TBL_MOVE_CARD.GetEntity(3);
+        if (data3st == null)
+        {
+            Debug.LogError("3번째 데이터 NUll");
+        }
+    }
+}
