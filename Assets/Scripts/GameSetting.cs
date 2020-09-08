a2f7ec59ce90aee0510c08cc8935e21d0907413c
GodHongIl <adunstudio@daum.net>
2020-09-09 03:18:01 +0900

    GameSetting.cs / TBL_STAGE

---
+++ Assets/Scripts/GameSetting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameSetting
+{
+     public static int PlayerStartHP     => TBL_GAME_SETTING.GetEntity(0).PlayerStartHP;
+
+     public static int PlayerStartEnergy => TBL_GAME_SETTING.GetEntity(0).PlayerStartEnergy;
+
+     public static float CardSelectTime  => TBL_GAME_SETTING.GetEntity(0).CardSelectTime;
+}
