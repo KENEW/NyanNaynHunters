069d92e161e428833bbd690722ab03736d175344
GodHongIl <adunstudio@daum.net>
2020-09-09 01:47:20 +0900

    기획: 가데이터 세팅 / CardSetting.cs / Enum:MoveType 이동

---
+++ Assets/Scripts/CardSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CardSetting
+{
+    public static int   MoveCardStartCount  => TBL_CARD_SETTINNG.GetEntity(0).MoveCardStartCount;
+    public static int   MoveCardMaxCount    => TBL_CARD_SETTINNG.GetEntity(0).MoveCardMaxCount;
+    public static float MoveCardDropPercent => TBL_CARD_SETTINNG.GetEntity(0).MoveCardDropPercent;
+    
+    public static int   AttackCardStartCount  => TBL_CARD_SETTINNG.GetEntity(0).AttackCardStartCount;
+    public static int   AttackCardMaxCount    => TBL_CARD_SETTINNG.GetEntity(0).AttackCardMaxCount;
+    public static float AttackCardDropPercent => TBL_CARD_SETTINNG.GetEntity(0).AttackCardDropPercent;
+    
+    public static int   GuardCardStartCount  => TBL_CARD_SETTINNG.GetEntity(0).GuardCardStartCount;
+    public static int   GuardCardMaxCount    => TBL_CARD_SETTINNG.GetEntity(0).GuardCardMaxCount;
+    public static float GuardCardDropPercent => TBL_CARD_SETTINNG.GetEntity(0).GuardCardDropPercent;
+    
+    public static int   EnergyCardStartCount  => TBL_CARD_SETTINNG.GetEntity(0).EnergyCardStartCount;
+    public static int   EnergyCardMaxCount    => TBL_CARD_SETTINNG.GetEntity(0).EnergyCardMaxCount;
+    public static float EnergyCardDropPercent => TBL_CARD_SETTINNG.GetEntity(0).EnergyCardDropPercent;
+    
+    public static int   HealCardStartCount  => TBL_CARD_SETTINNG.GetEntity(0).HealCardStartCount;
+    public static int   HealCardMaxCount    => TBL_CARD_SETTINNG.GetEntity(0).HealCardMaxCount;
+    public static float HealCardDropPercent => TBL_CARD_SETTINNG.GetEntity(0).HealCardDropPercent;
+}
--- Assets/Scripts/MoveCard.cs
+++ Assets/Scripts/MoveCard.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MoveType
+{
+    Down,
+    Up,
+    Left,
+    Right
+};
+
 public class MoveCard : Card
 {
     public int distance;
-    public enum MoveType
-    {
-        Down,
-        Up,
-        Left,
-        Right
-    };
+    
 }
