75d06f5bda97c43a218b00a9121ab1bdb0d0c0a6
wdmab1204 <wdmab1204@naver.com>
2020-09-09 10:35:59 +0900

    GameManager 추가
    GameManager 클래스 및 오브젝트 추가, round표시하는 텍스트 ui배치

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int round;
+    public Text roundText;
+    public Slider
+        playerHelathBar,
+        playerEnergyBar,
+        enemyHealthBar,
+        enemyEnergyBar;
+    //public Character player,enemy;
+    //public CardField cardField;
+
+    private void Start()
+    {
+        round = 1;
+        roundText.text = "Round : " + round.ToString();
+        //playerHelathBar.maxValue = player.maxHP;
+        //playerEnergyBar.maxValue = player.maxEnergy;
+        //enemyHealthBar.maxValue = enemy.maxHP;
+        //enemyEnergyBar.maxValue = enemy.maxEnergy;
+    }
+
+    //라운드 1,2...마다 호출되는 게임시작 함수
+    private void StartGame()
+    {
+        //if(player.hp <=0 && enemy.hp<=0) draw
+        //else if(player.hp<=0 && enemy.hp>0) win
+        //else lose
+    }
+}
