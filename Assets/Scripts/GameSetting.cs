068bcdd1d4252f46cdd6a5be7c476ea322cc2c99
GodHongIl <adunstudio@daum.net>
2020-09-10 21:12:17 +0900

    카드 실행 시간

--- Assets/Scripts/GameSetting.cs
+++ Assets/Scripts/GameSetting.cs
@@ -9,4 +9,11 @@ public static class GameSetting
      public static int PlayerStartEnergy => TBL_GAME_SETTING.GetEntity(0).PlayerStartEnergy;
 
      public static float CardSelectTime  => TBL_GAME_SETTING.GetEntity(0).CardSelectTime;
+     
+     public static float MoveCardTime     => TBL_GAME_SETTING.GetEntity(0).MoveCardTime;
+     public static float AttackCardTime   => TBL_GAME_SETTING.GetEntity(0).AttackCardTime;
+     public static float GuardCardTime    => TBL_GAME_SETTING.GetEntity(0).GuardCardTime;
+     public static float HealCardTime     => TBL_GAME_SETTING.GetEntity(0).HealCardTime;
+     public static float EnergyCardTime   => TBL_GAME_SETTING.GetEntity(0).EnergyCardTime;
+     
 }
