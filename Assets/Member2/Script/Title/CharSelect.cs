f16347b2a388274150f9ff23d14c181f47a06b71
Seunghyun <whtmdgus408@gmail.com>
2020-09-10 10:44:04 +0900

    타이틀 씬 구현, 사운드 오브젝트 구현(사운드 매니저)

---
+++ Assets/Member2/Script/Title/CharSelect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharSelect : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	public int CharIndex = 0;
+
+	bool enterCheck = false;
+	bool downCheck = false;
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		downCheck = true;
+	}
+
+	public void OnPointerEnter(PointerEventData data)
+	{
+		enterCheck = true;
+	}
+	public void OnPointerExit(PointerEventData data)
+	{
+		enterCheck = false;
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		if (enterCheck && downCheck)
+		{
+			//함수 전달
+
+
+			Debug.Log(CharIndex);
+		}
+		else
+		{
+			downCheck = false;
+		}
+	}
+}
---
+++ Assets/Member2/Script/Title/Title.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Title : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	public GameObject SelectPanel = null;
+	public void OnPointerDown(PointerEventData data)
+	{
+		
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		this.gameObject.SetActive(false);
+		SelectPanel.SetActive(true);
+	}
+}
