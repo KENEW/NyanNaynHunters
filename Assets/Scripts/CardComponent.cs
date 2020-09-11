2d684065b991f41112912b638231c3a3705c20aa
GodHongIl <adunstudio@daum.net>
2020-09-11 17:24:01 +0900

    ㅋㅇㅋㅇ

--- Assets/Scripts/CardComponent.cs
+++ Assets/Scripts/CardComponent.cs
@@ -13,6 +13,15 @@ public class CardComponent : MonoBehaviour
     private bool isUsed;//사용되면 true, 아니면 false
     public Image BackgroundImage;
 
+    public Image CardImage;
+
+
+    [Header("무브 이미지")] public Sprite m_MoveSprite;
+    [Header("어택 이미지")] public Sprite m_AttackSprite;
+    [Header("가드 이미지")] public Sprite m_GuardSprite;
+    [Header("힐 이미지")] public Sprite m_HealSprite;
+    [Header("에너지 이미지")] public Sprite m_EnergySprite;
+    
     private void Awake()
     {
         isUsed = false;
@@ -23,6 +32,40 @@ public class CardComponent : MonoBehaviour
         this.card = card;
         cardname = card.cardName;
         CardName.text = cardname;
+
+        //CardImage.enabled = true;
+      
+        /**
+        if (card is MoveCard)
+        {
+            CardImage.sprite = m_MoveSprite;
+        }
+
+        if (card is AttackCard)
+        {
+            CardImage.sprite = m_AttackSprite;
+        }
+
+        if (card is GuardCard)
+        {
+            CardImage.sprite = m_GuardSprite;
+        }
+
+        if (card is HealCard)
+        {
+            CardImage.sprite = m_HealSprite;
+        }
+
+        if (card is EnergyCard)
+        {
+            CardImage.sprite = m_EnergySprite;
+        }
+
+        if (card is EmptyCard)
+        {
+            CardImage.enabled = false;
+        }
+        **/
     }
 
     public void DeleteCard()
