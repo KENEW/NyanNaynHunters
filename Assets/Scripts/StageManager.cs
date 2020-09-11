659c04da8bf32c297bb080e4183e6b7fa8846698
GodHongIl <adunstudio@daum.net>
2020-09-11 14:12:40 +0900

    스테이지매니저

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoSingleton<StageManager>
+{
+    public SpriteRenderer m_SpriteRenderer;
+    
+    public List<Sprite> m_Backgrounds;
+
+    public int CurrentScene = 0;
+
+
+    public void Init()
+    {
+        CurrentScene = 0;
+    }
+
+    public void SetBackground()
+    {
+        m_SpriteRenderer.sprite = m_Backgrounds[CurrentScene];
+    }
+}
