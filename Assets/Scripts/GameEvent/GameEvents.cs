b477b3c5c0619652421e9bdb7dec07e88770496b
GodHongIl <adunstudio@daum.net>
2020-09-11 14:07:27 +0900

    게임 이벤트

---
+++ Assets/Scripts/GameEvent/GameEvents.cs
@@ -0,0 +1,20 @@
+﻿
+public enum GameEventType
+{
+    Open,
+}
+
+public struct GameEvent
+{
+    public GameEventType Type;
+    public int DataIndex;
+
+    private static GameEvent e;
+
+    public static void Trigger(GameEventType type)
+    {
+        e.Type = type;
+        
+        GameEventManager.TriggerGameEvent(e);
+    }
+}
